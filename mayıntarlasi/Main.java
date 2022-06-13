17a9ee1153d0e4fa8fde4779e32277b799a40837
stoneage12 <106374548+stoneage12@users.noreply.github.com>
2022-06-13 15:54:37 +0300

    Add files via upload
    www.patika.dev

---
+++ mayıntarlasi/Main.java
@@ -0,0 +1,12 @@
+package mayıntarlasi;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MayinTarlasi mayin = new MayinTarlasi(3,3);
+        mayin.run();
+
+    }
+}
